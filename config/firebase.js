3f27a623846a9b82e50ee10f0f8b6df5ca040ed6
GW1K <85939077+GW1K@users.noreply.github.com>
2022-02-02 03:37:55 +0100

    Add example firebase config

---
+++ config/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const firestore = getFirestore(app)
+export const auth = getAuth(app)
+export const storage = getStorage(app, firebaseConfig.storageBucket)
